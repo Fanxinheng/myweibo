5778378c98771ebe76ad88e8ff936d0dc3c0ad77
fanxinheng <xfan0701@163.com>
2017-11-15 23:36:43 +0800

    11.16

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    public function index ()
+    {
+    	return view('/admins/login/index');
+    }
+
+    public function login ()
+    {
+    	
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,4 +13,48 @@
 
 Route::get('/', function () {
     return view('welcome');
+    
 });
+
+
+//==========================后台路由===================================//
+
+//后台登录主页面
+Route::get('admin',"Admin\LoginController@index");
+
+//后台登录功能
+Route::get('admin/login',"Admin\LoginController@login");
+
+
+
+//后台主页面
+//中间件控制
+Route::group([],function(){
+
+	//后台主页
+	Route::resource('admin/index','Admin\UserController');
+
+	//后台管理员资源路由
+	Route::resource('admin/admins','Admin\AdminsController');
+
+	//后台热门微博资源路由
+	Route::resource('admin/hot','Admin\HotController');
+
+	//后台举报管理资源路由
+	Route::resource('admin/report','Admin\ReportController');
+
+	//后台广告管理资源路由
+	Route::resource('admin/advert','Admin\AdvertController');
+
+	//后台友情链接资源路由
+	Route::resource('admin/link','Admin\LinkController');
+
+	//后台系统公告资源路由
+	Route::resource('admin/notice','Admin\NoticeController');
+
+	//后台网站配置路由
+	Route::get('admin/config','Admin\ConfigController@edit');
+
+});
+
+
