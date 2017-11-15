f5321aa981eedfbda06e27785224085858e57a43
songwei9028 <33491677+songwei9028@users.noreply.github.com>
2017-11-15 16:48:47 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Welcome To Laravel 5</div>
+                <div class="title">Welcome To Laravel 5.11</div>
             </div>
         </div>
     </body>
